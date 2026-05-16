7562de40bbe346528e7a1c0d9d1cdff380c09a5c
divvysolar-dev <developer@divvysolar.in>
2026-05-16 15:45:50 +0530

    sitemaps changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -20,8 +20,18 @@ module.exports = {
             { loc: '/services/residential', changefreq: 'weekly', priority: 0.9 },
             { loc: '/services/industrial', changefreq: 'weekly', priority: 0.9 },
             { loc: '/services/utility-scale', changefreq: 'weekly', priority: 0.9 },
+            { loc: '/projects', changefreq: 'weekly', priority: 0.8 },
+            { loc: '/projects/haryana', changefreq: 'weekly', priority: 0.8 },
+            { loc: '/projects/punjab', changefreq: 'weekly', priority: 0.8 },
             { loc: '/blogs', changefreq: 'weekly', priority: 0.8 },
+            { loc: '/blogs/solar-potential-in-india-2025-key-trends-insights', changefreq: 'weekly', priority: 0.7 },
+            { loc: '/blogs/solar-power-is-the-future-2025', changefreq: 'weekly', priority: 0.7 },
+            { loc: '/blogs/epc-solutions-in-solar-energy-2025', changefreq: 'weekly', priority: 0.7 },
             { loc: '/contact', changefreq: 'monthly', priority: 0.8 },
+            { loc: '/careers', changefreq: 'weekly', priority: 0.7 },
+            { loc: '/careers/people-stories', changefreq: 'weekly', priority: 0.7 },
+            { loc: '/calculator', changefreq: 'weekly', priority: 0.7 },
+            { loc: '/sustainability-calculator', changefreq: 'weekly', priority: 0.7 },
         ]
     },
 }
